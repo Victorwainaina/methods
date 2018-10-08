162a418138213fe1f7e20f9d6f5e6cb793d46cb0
Victor Wainaina <victor.kiragu17@students .dkut.ac.ke>
2018-10-08 12:07:15 +0300

    Add project files.

---
+++ methods/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace methods
+{
+    //this is a simple voting system progran
+    internal static class Program
+    {
+        private static void Main()
+        {
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Title = "Methods";
+            Console.WriteLine("Enter the number of members to participate in the voting");
+            var part = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+            List<string> num;
+            num = new List<string>();
+            for (var voted = 0; voted < part; voted++)
+            {
+                Console.Write($"{voted} : ");
+                num.Add(Console.ReadLine());
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
